3284de98f1a7559cbb098bfe2b5d0dc66d9b4d00
tonyp7 <contact@tonypottier.info>
2017-02-19 21:03:19 +0800

    comment error fix

--- NixieCount/NixieCount.ino
+++ NixieCount/NixieCount.ino
@@ -35,7 +35,7 @@ void setup() {
 
 void nixieWrite(uint8_t a, uint8_t b, uint8_t c, uint8_t d, uint8_t value){
   //D is most significant bit
-  //A is lead significant bit
+  //A is least significant bit
   digitalWrite(d, (value & 0x08) >> 3);
   digitalWrite(c, (value & 0x04) >> 2);
   digitalWrite(b, (value & 0x02) >> 1);
