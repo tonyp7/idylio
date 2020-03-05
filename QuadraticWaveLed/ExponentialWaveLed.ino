4a8f74b3caf3f623ba7bbc813bcb9732cd97b627
prosper00 <57373245+prosper00@users.noreply.github.com>
2020-03-05 13:43:50 -0700

    Update ExponentialWaveLed.ino

--- QuadraticWaveLed/ExponentialWaveLed.ino
+++ QuadraticWaveLed/ExponentialWaveLed.ino
@@ -1,6 +1,15 @@
+/*
+Example code implementing the ExponentialWaveLed function.
+Works on an STM32F103 'blue pill' board.
+
+Also implements a LED 'chaser' of 8 leds on 8 GPIO's
+And demonstrates how to 'multitask' the 'wave' and
+'chase' functions
+*/
+
 #include "ExponentialWaveLed.h"
-  
-/*Here's where we define which of the pin number our LEDs are connected to*/
+
+/*Here's where we define which of the pins our 'chaser' LEDs are connected to*/
 const int LEDS[8]={
   PB9,
   PB8,
@@ -12,10 +21,10 @@ const int LEDS[8]={
   PA15
 };
 
-#define WHITELED A0
+#define WHITELED A0  // this is the LED we'll be 'waving'
 
 ExponentialWaveLed exponentialWaveLed;
-const unsigned long WAVE_LEN = 2000UL;
+const unsigned long WAVE_LEN = 2000UL;  //2 second 'wave' time
 unsigned long milli = 0UL;
 
 // the setup function runs once when you press reset or power the board
@@ -28,21 +37,22 @@ void setup() {
   }
   pinMode(LED_BUILTIN, OUTPUT);
   digitalWrite(LED_BUILTIN,HIGH); 
+  
+  //initialize our wave routine
   exponentialWaveLed.setup(WHITELED, WAVE_LEN);
 
 //  Serial.begin(115200);
 }
 
-// the loop function runs over and over again forever
 void loop() { 
-  for(int i=0; i<=7; i++){    
+  for(int i=0; i<=7; i++){    // loop through each of our chaser segment LED's
     digitalWrite(LEDS[i],!digitalRead(LEDS[i]));
-    for(int j=0; j<=19; j++){
+    for(int j=0; j<=19; j++){  //How long do we keep our 'chaser' segment lit
       delay(1);
-      exponentialWaveLed.update(millis());
+      exponentialWaveLed.update(millis()); //keep on updating our wave LED 
     }
     digitalWrite(LEDS[i],!digitalRead(LEDS[i]));
-    for(int j=0; j<=19; j++){
+    for(int j=0; j<=19; j++){ // how long do we keep our 'chaser' segment 'off' for
       delay(1);
       exponentialWaveLed.update(millis());
     }
